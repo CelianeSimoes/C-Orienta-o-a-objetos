da909e93c0e6e5a35f47b74a5dee07a635bf2fd4
Celiane Simoes <celi_bonifacio@hotmail.com>
2021-08-10 23:03:06 -0300

    Revisado entrada e saida de dados

--- Segundo Projeto/Segundo Projeto/Program.cs
+++ Segundo Projeto/Segundo Projeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Segundo_Projeto
 {
@@ -6,7 +7,30 @@ namespace Segundo_Projeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Resolução exercício de fixação");
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preço1 = 2100.0;
+            double preço2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é ${preço1}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preço2}");
+
+
+            Console.WriteLine("Registro: {0} anos de idade, código {1} e genero: {2}", idade,codigo, genero);
+
+
+            Console.WriteLine(" Medida com oito casas decimais:" + medida.ToString("f8"));
+            Console.WriteLine(" Arredondado (três casas decimais): " + medida.ToString("f3"));
+            Console.WriteLine("Separador decimal invariant culture" + medida.ToString("f3", CultureInfo.InvariantCulture));
         }
     }
 }
