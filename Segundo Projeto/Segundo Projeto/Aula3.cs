102c134d136a95778f128be9e668837b208e1692
Celiane Simões <celi_bonifacio@hotmail.com>
2021-08-13 23:12:36 -0300

    Realizados exercícios aplicando conceito de membros estáticos

--- Segundo Projeto/Segundo Projeto/Aula3.cs
+++ Segundo Projeto/Segundo Projeto/Aula3.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+using System.Globalization;
+
+namespace Segundo_Projeto
+{
+    static class Aula3
+    {
+        static void MetodoDaAula()
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Fazer um programa para ler o nome e salário de dois funcionários. Depois mostrar o salário médio dos funcionários. ");
+
+            Console.WriteLine("Insira os dados do primeiro colaborador:");
+            Console.WriteLine("Nome:");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Insira os dados do segundo colaborador:");
+            Console.WriteLine("Nome:");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
+
+
---
+++ Segundo Projeto/Segundo Projeto/Aula4.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Segundo_Projeto
+{
+    static class Aula4
+    {
+        
+        public static void MetodoDaAula()
+        {
+
+
+
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a serem adicionados ao estoque ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine("Exercício proposto - aplicação de mebros estáticos");
+            Console.WriteLine("Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor de uma circunferência e do volume de uma esfera para raio daqueke valor. Informar também o valor de PI com duas casas decimais");
+            Console.WriteLine("Versão 1");
+
+            
+            Console.WriteLine("entre como valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Criada segunda versão utilizando a classe calculadora");
+            Console.WriteLine("Criada terceira versão incluindo STATIC  na classe e alterando o código chamando diretamente a classe");
+
+
+        }
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+}
