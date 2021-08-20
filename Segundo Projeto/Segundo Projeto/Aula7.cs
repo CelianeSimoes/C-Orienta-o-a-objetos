5c98727342647f1f6bd2536096a151c7435d9883
Celiane Simões <celi_bonifacio@hotmail.com>
2021-08-19 23:44:22 -0300

    Aplicado tópicos especiais de C#

---
+++ Segundo Projeto/Segundo Projeto/Aula7.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Segundo_Projeto
+{
+    static class Aula7
+    {
+       
+        public static void MetodoDaAula()
+        {
+            Estudante[] vect = new Estudante[10];
+            Console.Write("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+            double aluguel = 230.00;
+            double total;
+
+            
+            
+            for (int i = 1; i<= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i} : ");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.Write("Tempo de contrato/locação em meses: ");
+                int tempo = int.Parse(Console.ReadLine());
+                 total = (aluguel * tempo);
+                Console.WriteLine("Total valor do contrato: " + total.ToString("f2"), CultureInfo.InvariantCulture);
+                vect[quarto] = new Estudante (nome, email, aluguel, total);
+                
+
+            }
+            Console.WriteLine();
+            Console.Write("Quartos ocupados:");
+
+            for (int i = 1; i <= 10; i++)
+            
+                if (vect [i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect [i] );
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ Segundo Projeto/Segundo Projeto/Estudante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Segundo_Projeto
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int TempoContrato { get; set; }
+
+        public double Aluguel { get; private set; }
+        public double Total { get; private set; }
+
+        public Estudante (string nome, string email, double aluguel, double total)
+        {
+            Nome = nome;
+            Email = email;
+            Aluguel = aluguel;
+            Total = total;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
