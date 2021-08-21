003b2d8b51a95707ba5a2888397a321617f429ff
Celiane Simões <celi_bonifacio@hotmail.com>
2021-08-20 23:35:02 -0300

    Iniciado conceito e aplicação de Herança

---
+++ Segundo Projeto/Segundo Projeto/Aula8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Segundo_Projeto
+{
+    static class Aula8
+
+    {
+        
+        public static void MetodoDaAula()
+        {
+            DateTime d1 = new DateTime(2020, 8, 15, 19, 13, 57, DateTimeKind.Local);
+
+            Console.WriteLine(d1);
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Segundo Projeto/Segundo Projeto/Entities/BusinessAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Segundo_Projeto.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+        public BusinessAccount (int number, string holder,double balance,double loanLimit):base(number,holder,balance)
+        {
+            
+            LoanLimit = LoanLimit;
+        }
+       public void Loan(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
