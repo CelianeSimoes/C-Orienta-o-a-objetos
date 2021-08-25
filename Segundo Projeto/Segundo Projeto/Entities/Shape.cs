6d345046858772059860663f4176bc3e6cd66fd4
Celiane Simões <celi_bonifacio@hotmail.com>
2021-08-24 22:13:03 -0300

    Aplicado conceito de Polimorfismo,Classes e metodos abstratos

---
+++ Segundo Projeto/Segundo Projeto/Entities/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Segundo_Projeto.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+        public abstract double Area(); 
+
+
+    }
+
+
+
+}
