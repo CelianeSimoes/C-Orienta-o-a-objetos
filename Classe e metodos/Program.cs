5cd2f5efbef212e204242f5e76e16215c4486c7d
Celiane Simoes <celi_bonifacio@hotmail.com>
2021-09-08 20:09:39 -0300

    criada classe.

---
+++ Classe e metodos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classe_e_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
